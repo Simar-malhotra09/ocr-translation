be77619fe5fdd7da5423de7400ca86013326bce2
Simar Malhotra <malhotrarajsimar09@gmail.com>
2024-03-09 19:13:21 -0500

    added text extraction

---
+++ main.py
@@ -0,0 +1,17 @@
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, "rb") as image_file:
+        content = image_file.read()
+        image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    print("Texts:")
+    print(texts[0].description)
+
+detect_text("./images/soda.jpeg")
