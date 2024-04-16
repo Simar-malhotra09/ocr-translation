d8be5f56d42bb09488b7e4f1dc75c6f3258bb52f
Simar Malhotra <malhotrarajsimar09@gmail.com>
2024-04-16 01:15:05 -0400

    added comments

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ to-do:
 1. Can I train/finetune a model on jp images for text extraction.(https://www.kaggle.com/datasets/hnthnt/jp-font-image-dataset-02)
 2. ocr paper (https://arxiv.org/pdf/1801.01316.pdf)
 '''
+
+''' 
+5/16/24:
+Not too bad, the translation should be decent if you can extract well.
+For manga, its kinda weird 
+'''
+
 def detect_text(path):
     """Detects text in the file."""
     from google.cloud import vision
